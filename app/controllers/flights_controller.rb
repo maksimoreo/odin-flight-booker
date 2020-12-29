ca6f05bf3da13abe3f780a7daddf8b57b9f3162a
maksim <maximthebest@outlook.com>
2020-12-29 20:45:08 +0200

    Flight search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,9 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all.includes(:from_airport, :to_airport)
+    if params[:commit] == 'Search'
+      @flights = Flight.where(from_airport: Airport.find_by_name(params[:from]), to_airport: Airport.find_by_name(params[:to]), start: params[:date]).all
+    else
+      @flights = Flight.all.includes(:from_airport, :to_airport)
+    end
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :from_airport, class_name: 'Airport'
   belongs_to :to_airport, class_name: 'Airport'
+
+  def self.distinct_dates
+    Flight.select(:start).distinct.pluck(:start)
+  end
 end
